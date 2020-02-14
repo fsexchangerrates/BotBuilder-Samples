261097c2c8d4c8745e16c3ecca712c0e7e80dba4
fsexchangerrates <fs.suppermaster@gmail.com>
2020-02-15 03:22:40 +0700

    updates

--- samples/javascript_nodejs/15.handling-attachments/bots/flexMessageCards.js
+++
@@ -1,6 +0,0 @@
-const { CardAction, CardFactory, MessageFactory, ActionTypes, ActivityHandler } = require('botbuilder');
-class FlexMessageCards implements AttachmentsBot {
-    constructor(MessageFactory) {
-        
-    }
-}
---
+++ samples/javascript_nodejs/15.handling-attachments/bots/handleMessage.js
@@ -0,0 +1,17 @@
+const { CardAction, CardFactory, MessageFactory, ActionTypes, ActivityHandler } = require('botbuilder');
+const ChannelData1 = require('../channelDatas/ChannelData1.json');
+const ChannelData2 = require('../channelDatas/ChannelData2.json');
+
+class HandleMessage {
+    /**
+     * @param {Array} channelDatas
+     * @param {Object} channelData1
+     * @param {Object} channelData2
+     */
+
+     constructor(channelDatas) {
+        this.channelDatas = channelDatas;
+
+         super();
+     }
+}
